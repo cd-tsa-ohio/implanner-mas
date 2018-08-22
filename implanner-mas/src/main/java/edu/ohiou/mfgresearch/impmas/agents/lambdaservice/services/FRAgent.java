30b06d373b72baa9e9515e595fe5dece19dc19ec
sormaz <sormaz@gmail.com>
2018-08-22 10:28:12 -0400

    Arko's files

--- implanner-mas/src/main/java/edu/ohiou/mfgresearch/impmas/agents/lambdaservice/services/FRAgent.java
+++ implanner-mas/src/main/java/edu/ohiou/mfgresearch/impmas/agents/lambdaservice/services/FRAgent.java
@@ -4,7 +4,7 @@ import edu.ohiou.mfgresearch.implanner.parts.MfgPartModel;
 import edu.ohiou.mfgresearch.impmas.agents.LambdaAgent;
 import edu.ohiou.mfgresearch.impmas.semantics.PartFile;
 import edu.ohiou.mfgresearch.impmas.semantics.ServiceAction;
-import edu.ohiou.mfgresearch.lambda.Anything;
+import edu.ohiou.mfgresearch.lambda.Uni;
 import jade.content.onto.basic.Action;
 import jade.lang.acl.ACLMessage;
 
@@ -22,23 +22,23 @@ public class FRAgent extends LambdaAgent {
 		 * accepted concrete concept: property .input
 		 *   	
 		 */
-		messageProcessor = s->{
-			
-			 return Anything.of(s)
-			 		 .filter(m->m.getPerformative()==ACLMessage.QUERY_REF?true:false)
-			 		 .fMap(m->Anything.of(getContentManager().extractContent(m)))
-			 		 .fMap(a->Anything.of(((Action) a).getAction()))
-			 		 .fMap(q->Anything.of(((ServiceAction) q).getInputObject()))
-			 		 .filter(c->Class.forName((String) property.get(getClass().getName()+".input")).isInstance(c));
-			 
-		};
+//		messageProcessor = s->{
+////			
+////			 return Algo.of(s)
+////			 		 .filter(m->m.getPerformative()==ACLMessage.QUERY_REF?true:false)
+////			 		 //.fMap(m->Algo.of(getContentManager().extractContent(m)))
+////			 		 //.fMap(a->Algo.of(((Action) a).getAction()))
+////			 		 //.fMap(q->Algo.of(((ServiceAction) q).getInputObject()))
+////			 		 .filter(c->Class.forName((String) property.get(getClass().getName()+".input")).isInstance(c));
+//			 
+//		};
 		
 		/**
 		 * FeatureMapping service
 		 */
 		service = i->{
 			
-			return Anything.of(i)
+			return Uni.of(i)
 					.map(s->((PartFile) i).getTempFile())
 					.map(MfgPartModel::openUGFile)
 					.get();
